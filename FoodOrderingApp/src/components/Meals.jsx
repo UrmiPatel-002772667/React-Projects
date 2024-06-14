3736f70fa3fffa8fed3e10e4819b26e6d06572be
Urmi Patel <patel.urmi@northeastern.edu>
2024-06-14 15:01:09 -0400

    Adding Food Ordering App Project

---
+++ FoodOrderingApp/src/components/Meals.jsx
@@ -0,0 +1,25 @@
+import MealItem from './MealItem.jsx';
+import useHttp from '../Hooks/useHttp.js';
+import Error from './Error';
+
+const requestConfig = {};
+
+export default function Meals() {
+  const {data: loadedMeals, isLoading, error} = useHttp('http://localhost:3000/meals', requestConfig, [])
+  
+  if(isLoading){
+    return <p className='center'> Fetching Data....</p>
+  }
+
+  if(error){
+    return <Error title="Failed to fetch meals" message={error}/>
+  }
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
+    </ul>
+  );
+}
